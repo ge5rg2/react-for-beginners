9fb8e74f19846e8596896624b752aac60b38748d
George <kghee9612@naver.com>
2022-01-25 10:32:41 +0900

    Deps

--- src/Button.js
+++ src/Button.js
@@ -4,15 +4,25 @@ import styles from "./Button.module.css";
 
 const Button = ({ text }) => {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   const onClick = () => {
     setCounter((prev) => prev + 1);
-    console.log("render");
   };
+  console.log("I'm run all time");
   useEffect(() => {
-    console.log("just once");
+    console.log("call API");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input onChange={onChange} type="text" placeholder="Search here..." />
       <button onClick={onClick} className={styles.btn}>
         {text}
       </button>
