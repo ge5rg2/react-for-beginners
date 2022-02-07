cb1a98df8b2c6c92521cce0067223773dc044d45
George <kghee9612@naver.com>
2022-02-07 17:24:08 +0900

    Coin price tracker2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const saveMoneyValue = (event) => {
+    setMoney(event.target.value);
+  };
+  coins.splice(100, coins.length - 100);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -17,13 +22,27 @@ function App() {
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <div>
+          <h3>How much do you have?</h3>
+          <form>
+            <input
+              onChange={saveMoneyValue}
+              value={money}
+              type="number"
+              min="0"
+            />
+            {" $"}
+          </form>
+          <hr />
+          <h3>You can buy...</h3>
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
