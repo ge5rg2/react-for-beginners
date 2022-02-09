890fd03207ca8a39c1cbca0679a8dcf390958e0d
George <kghee9612@naver.com>
2022-02-09 17:43:38 +0900

    movie app pt1

--- src/App.js
+++ src/App.js
@@ -2,80 +2,38 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [buy, setBuy] = useState(false);
-  const handleBuyClick = (event) => {
-    event.preventDefault();
-    setBuy((current) => !current);
-    setPrice(false);
+  /* [1,2] 1이 배열의 데이터, 2가 배열 데이터를 수정하는 함수 */
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  const [price, setPrice] = useState(false);
-  const handlePriceClick = (event) => {
-    event.preventDefault();
-    setPrice((current) => !current);
-    setBuy(false);
-  };
-  const [money, setMoney] = useState(0);
-  const saveMoneyValue = (event) => {
-    setMoney(event.target.value);
-  };
-  coins.splice(100, coins.length - 100);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <form>
-            <button onClick={handleBuyClick}>Buy Coins</button>
-            <button onClick={handlePriceClick}>Market price</button>
-          </form>
-          {buy ? (
-            <div>
-              <h3>How much do you have?</h3>
-              <form>
-                <input
-                  onChange={saveMoneyValue}
-                  value={money}
-                  type="number"
-                  min="0"
-                />
-                {" $"}
-              </form>
-              <hr />
-              <h3>You can buy...</h3>
-              <select>
-                {coins.map((coin) => (
-                  <option key={coin.id}>
-                    {money / coin.quotes.USD.price} / {coin.name} ({coin.symbol}
-                    )
-                  </option>
-                ))}
-              </select>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres == null
+                  ? ""
+                  : movie.genres.map((i) => <li key={i}>{i}</li>)}
+              </ul>
             </div>
-          ) : null}
-          {price ? (
-            <div>
-              <h3>Market Price</h3>
-              <hr />
-              <select>
-                {coins.map((coin) => (
-                  <option key={coin.id}>
-                    {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-                  </option>
-                ))}
-              </select>
-            </div>
-          ) : null}
+          ))}
         </div>
       )}
     </div>
